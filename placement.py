451aaf5138a2cc3cbc2cdc754c71f9cf93462969
HyunSeo Jung <hsjung02@postech.ac.kr>
2023-05-26 05:12:44 +0900

    main.py

--- placement.py
+++ placement.py
@@ -1,16 +1,10 @@
 # %%
 
-adjacency_matrix = [[0,1,1,0],[1,0,0,1],[1,0,0,0],[0,1,0,0]]
-cells = {0:{'width':8,'height':8}, 1:{'width':6,'height':6},2:{'width':4,'height':4},3:{'width':2,'height':2}}
-macro_indices = [0,1,2,3]
-std_indices = []
-pin_indices = []
+from utils.parsing import load_netlist
+adjacency_matrix, cells, macro_indices, std_indices, pin_indices = load_netlist("./netlist")
 
 from environment.environment import CircuitEnv
-env = CircuitEnv(adjacency_matrix, cells, macro_indices, std_indices, pin_indices, reward_weights=[1,0,10])
-
-# from stable_baselines3.common.env_checker import check_env
-# check_env(env)
+env = CircuitEnv(adjacency_matrix, cells, macro_indices, std_indices, pin_indices, reward_weights=[1,0])
 
 from sb3_contrib import MaskablePPO
 from sb3_contrib.common.maskable.utils import get_action_masks
@@ -21,17 +15,37 @@ model.policy
 
 done = False
 
-obs = env.reset()[0]
+# %%
+# Before training
+
+obs, _ = env.reset()
 while not done:
     action_masks = get_action_masks(env)
     action, _states = model.predict(obs, action_masks=action_masks)
-    print(action)
     obs, reward, done, truncated, info = env.step(action)
+env.render()
 
+
+# %%
+# After training
+
+model.learn(100)
+
+obs, _ = env.reset()
+while not done:
+    action_masks = get_action_masks(env)
+    action, _states = model.predict(obs, action_masks=action_masks)
+    obs, reward, done, truncated, info = env.step(action)
 env.render()
 
 # %%
-env.place_std()
+obs, _ = env.reset()
+while not done:
+    action_masks = get_action_masks(env)
+    action, _states = model.predict(obs, action_masks=action_masks)
+    obs, reward, done, truncated, info = env.step(action)
 env.render()
 # %%
+model.save("placement.pt")
 
+# %%

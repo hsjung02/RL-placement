9d49d2007a9ea39d68dfde1c2b71c1b985a09b8f
hs_server <hsjung02@postech.ac.kr>
2023-05-27 20:27:37 +0900

    main.py

--- placement.py
+++ placement.py
@@ -8,22 +8,31 @@ from sb3_contrib.common.maskable.utils import get_action_masks
 from model.agent import CircuitExtractor, CircuitActorCriticPolicy
 from utils.callback import ProgressCallback, VideoRecorderCallback
 from stable_baselines3.common.monitor import Monitor
+from time import time
 
-if th.cuda.is_available():
-    th.set_default_tensor_type("th.cuda.FloatTensor")
-# dtype = "th.cuda.FloatTensor" if th.cuda.is_available() else "th.FloatTensor"
-# th.set_default_tensor_type(dtype)
+dtype = "torch.cuda.FloatTensor" if th.cuda.is_available() else "torch.FloatTensor"
+th.set_default_tensor_type(dtype)
 
 
 adjacency_matrix, cells, macro_indices, std_indices, pin_indices = load_netlist("./netlist")
 env = CircuitEnv(adjacency_matrix, cells, macro_indices, std_indices, pin_indices, reward_weights=[1,0])
-log_env = CircuitEnv(adjacency_matrix, cells, macro_indices, std_indices, pin_indices, reward_weights=[1,0])
+log_env = CircuitEnv(adjacency_matrix, cells, macro_indices, std_indices, pin_indices, reward_weights=[1,0])]
 log_env = Monitor(log_env)
+print("made environments")
+
+n_steps = 40
+batch_size = 20
+total_timesteps = 3000
 
 policy_kwargs = dict(features_extractor_class=CircuitExtractor)
-model = MaskablePPO(CircuitActorCriticPolicy, env, n_steps=1, batch_size=1, policy_kwargs=policy_kwargs,verbose=1, tensorboard_log="./placement_tensorboard/")
-model.policy
-
+model = MaskablePPO(policy=CircuitActorCriticPolicy,
+                    env=env,
+                    n_steps=n_steps,
+                    batch_size=batch_size,
+                    verbose=0,
+                    policy_kwargs=policy_kwargs,
+                    tensorboard_log="./placement_tensorboard/")
+print("Model setting done")
 # %%
 # Before training
 
@@ -35,16 +44,18 @@ while not done:
     obs, reward, done, truncated, info = env.step(action)
 env.render()
 
-
+print("Start training")
+start_time = time()
 # %%
 # After training
 
 callback = ProgressCallback(check_freq=100)
 # callback = VideoRecorderCallback(log_env, render_freq=1)
 
-model.learn(total_timesteps=1000, callback=callback)
+model.learn(total_timesteps=total_timesteps, callback=callback)
 # model.learn(total_timesteps=1000)
 
+print("Finish training in %ds"%(time()-start_time))
 obs, _ = env.reset()
 done = False
 while not done:
@@ -53,4 +64,4 @@ while not done:
     obs, reward, done, truncated, info = env.step(action)
 env.render()
 # %%
-model.save("placement.pt")
+model.save("placement_nsteps%d_batch%d"%(n_stseps, batch_size))
